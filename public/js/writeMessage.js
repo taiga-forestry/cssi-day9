ceb4753f8dee34a15a72b88d291b588fdec71f18
Tiger Ji <tiger.j540@gmail.com>
2021-07-28 17:07:13 +0000

    saving

--- public/js/writeMessage.js
+++ public/js/writeMessage.js
@@ -2,6 +2,7 @@ submitMessage = () => {
     const passcode = document.querySelector("#passcode").value;
     const message = document.querySelector("#message").value;
 
+    console.log(passcode);
     console.log(message.length)
     if (message.length > 50 ) {
         alert("Message is too long. Enter a shorter one!");
@@ -32,12 +33,11 @@ passwordSecure = (password) => {
 }
 
 passwordHasNumber = (password) => {
-    const numbers = [0,1,2,3,4,5,6,7,8,9];
-    
-    for (let num in numbers){
-        if (password.includes(num)){
-            console.log(num);
-            // console.log(numbers);
+    const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+    console.log(numbers[0])
+    for (let i = 0; i < numbers.length; i++){
+        if (password.includes(numbers[i])){
+            console.log(numbers[i]);
             return true;
         }
     }
@@ -48,11 +48,11 @@ passwordHasNumber = (password) => {
 passwordHasCapital = (password) => {
     const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     
-    for (let i = 0; i < password.length; i++){
+    for (let i = 0; i < letters.length; i++){
+        console.log(letters[i])
         if (password.includes(letters[i]))
         {   
             console.log(letters[i]);
-            // console.log(letters);
             return true;
         }
     }
